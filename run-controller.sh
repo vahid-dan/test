88954a3da3d7a0e54df7fd8a92faa5f4e219c6a8
Vahid Daneshmand <v.daneshmand@gmail.com>
2018-10-30 11:58:21 -0400

    Update run-controller.sh

--- run-controller.sh
+++ run-controller.sh
@@ -4,5 +4,7 @@ echo $SNAP
 echo $SNAP_DATA
 echo $SNAP_USER_DATA
 
+sudo ovs-vsctl add-br snapipop 2>&1 | tee $SNAP_USER_DATA/ovs.log
+
 cd $SNAP
 python3 -m controller.Controller -c $SNAP_USER_DATA/config.json

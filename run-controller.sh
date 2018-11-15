846f2ccdcd36a3c28436845d65fb3c0e0ce3244b
Vahid Daneshmand <v.daneshmand@gmail.com>
2018-11-15 10:21:25 -0500

    Update run-controller.sh

--- run-controller.sh
+++ run-controller.sh
@@ -9,8 +9,11 @@ sudo echo "sudo echo successfull"
 ifconfig
 
 sudo $SNAP/usr/bin/ovs-vsctl add-br snapipop 2>&1 | tee $SNAP_USER_DATA/ovs.log
+sudo $SNAP/bin/ovs-vsctl add-br snapipop0 2>&1 | tee -a $SNAP_USER_DATA/ovs.log
+$SNAP/bin/ovs-vsctl add-br snapipop1 2>&1 | tee -a $SNAP_USER_DATA/ovs.log
 sudo usr/bin/ovs-vsctl add-br snapipop 2>&1 | tee -a $SNAP_USER_DATA/ovs.log
 sudo ovs-vsctl add-br snapipop 2>&1 | tee -a $SNAP_USER_DATA/ovs.log
+ovs-vsctl add-br snapipop2 2>&1 | tee -a $SNAP_USER_DATA/ovs.log
 which ovs-vsctl
 
 cd $SNAP

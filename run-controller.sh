37a269ed0295bdb1b89f1f166ab61f94ef83c0a5
Vahid Daneshmand <v.daneshmand@gmail.com>
2018-11-15 10:48:17 -0500

    Update run-controller.sh

--- run-controller.sh
+++ run-controller.sh
@@ -6,14 +6,10 @@ echo $SNAP_USER_DATA
 
 sudo echo "sudo echo successfull"
 
-ifconfig
-
-sudo $SNAP/usr/bin/ovs-vsctl add-br snapipop 2>&1 | tee $SNAP_USER_DATA/ovs.log
-sudo $SNAP/bin/ovs-vsctl add-br snapipop0 2>&1 | tee -a $SNAP_USER_DATA/ovs.log
-$SNAP/bin/ovs-vsctl add-br snapipop1 2>&1 | tee -a $SNAP_USER_DATA/ovs.log
-sudo usr/bin/ovs-vsctl add-br snapipop 2>&1 | tee -a $SNAP_USER_DATA/ovs.log
-sudo ovs-vsctl add-br snapipop 2>&1 | tee -a $SNAP_USER_DATA/ovs.log
-ovs-vsctl add-br snapipop2 2>&1 | tee -a $SNAP_USER_DATA/ovs.log
+sudo $SNAP/bin/ovs-vsctl add-br snapipop00 2>&1 | tee -a $SNAP_USER_DATA/ovs.log
+$SNAP/bin/ovs-vsctl add-br snapipop11 2>&1 | tee -a $SNAP_USER_DATA/ovs.log
+sudo ovs-vsctl add-br snapipop22 2>&1 | tee -a $SNAP_USER_DATA/ovs.log
+ovs-vsctl add-br snapipop33 2>&1 | tee -a $SNAP_USER_DATA/ovs.log
 which ovs-vsctl
 
 cd $SNAP

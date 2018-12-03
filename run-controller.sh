fcbac054c2e513ec1adad82e317ef0e8ee89653b
Vahid Daneshmand <v.daneshmand@gmail.com>
2018-12-03 12:08:10 -0500

    Update run-controller.sh

--- run-controller.sh
+++ run-controller.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 
-echo $SNAP
-echo $SNAP_DATA
-echo $SNAP_USER_DATA
-
-sudo echo "sudo echo successfull"
-
-$SNAP/bin/ovs-vsctl add-br snapipoptest0 2>&1 | tee -a $SNAP_USER_DATA/ovs.log
-ovs-vsctl add-br snapipoptest1 2>&1 | tee -a $SNAP_USER_DATA/ovs.log
-which ovs-vsctl
-
 cd $SNAP
 python3 -m controller.Controller -c $SNAP_USER_DATA/config.json
